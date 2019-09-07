874649091a34280c24deabe1777529ef44d3d572
BillyZou <yz3352@nyu.edu>
2019-09-07 14:02:49 +0800

    feat: disable uglifying in dev mode

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,6 +4,9 @@ const common = require('./webpack.common');
 
 module.exports = merge(common, {
   mode: 'development',
+  optimization:{
+    minimize: false,
+  },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
   ],
